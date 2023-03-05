69c04c7410e1e74e95e94286aad09f19eb8144f1
Alexandru Stefan <alex@ndru.dev>
2023-03-05 22:38:12 +0700

    feat: add default tests

--- tests/solana-calculator.ts
+++ tests/solana-calculator.ts
@@ -1,16 +1,48 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+const { SystemProgram } = anchor.web3
+import { expect } from 'chai';
 import { SolanaCalculator } from "../target/types/solana_calculator";
 
+//Moka works using predescribed it blocks 
 describe("solana-calculator", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
+  //Referencing the program - Abstraction that allows us to call methods of our SOL program.
   const program = anchor.workspace.SolanaCalculator as Program<SolanaCalculator>;
+  const programProvider = program.provider as anchor.AnchorProvider;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  //Generating a keypair for our Calculator account
+  const calculatorPair = anchor.web3.Keypair.generate();
+
+  const text = "Summer School Of Solana"
+
+  //Creating a test block
+  it("Creating Calculator Instance", async () => {
+    //Calling create instance - Set our calculator keypair as a signer
+    await program.methods.create(text).accounts(
+      {
+          calculator: calculatorPair.publicKey,
+          user: programProvider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+      }
+    ).signers([calculatorPair]).rpc()
+
+    //We fecth the account and read if the string is actually in the account
+    const account = await program.account.calculator.fetch(calculatorPair.publicKey)
+    expect(account.greeting).to.eql(text)
   });
+
+  //Another test step - test out addition
+  it('Addition',async () => {
+    await program.methods.add(new anchor.BN(2), new anchor.BN(3))
+      .accounts({
+          calculator: calculatorPair.publicKey,
+      })
+      .rpc()
+    const account = await program.account.calculator.fetch(calculatorPair.publicKey)
+    expect(account.result).to.eql(new anchor.BN(5))
+  })
+
 });
