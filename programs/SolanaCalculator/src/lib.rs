b5d9a416c940490af58be707985938f6ba34c418
Alexandru Stefan <alex@ndru.dev>
2023-03-05 17:13:42 +0700

    feat: publish program

--- programs/SolanaCalculator/src/lib.rs
+++ programs/SolanaCalculator/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("2BDRQuuxdr2kCMN4z7cfbgpcv98bLCF5kCeJkwBDTnyZ");
 
 #[program]
 pub mod solana_calculator {
